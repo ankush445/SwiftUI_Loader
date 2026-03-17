e5056bafebce140b2f782a966cc0dc01ddf265ed
Ankush Miittal <sfs.ankush22@gmail.com>
2026-03-17 16:25:12 +0530

    Initial commit

---
+++ Sources/SwiftUI_Loader/SwiftUI_Loader.swift
@@ -0,0 +1,53 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct LoadingModifier: ViewModifier {
+    @Environment(\.colorScheme) var colorScheme
+    var isLoading: Bool
+    var loadingText: String
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+                .disabled(isLoading) // Disable interactions when loading
+            
+            if isLoading {
+                GeometryReader { geometry in
+                    Color.black.opacity(0.2)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                    
+                    VStack {
+                        Spacer()
+                        VStack {
+                            ProgressView(loadingText)
+                                .progressViewStyle(CircularProgressViewStyle())
+                                .controlSize(.large)
+                                .tint(colorScheme == .dark ? .white : .gray)
+                                .padding()
+                        }
+                        .padding(.horizontal, 30)
+                        .padding(.vertical)
+                        .background(
+                            loadingText != ""
+                                ? Color(uiColor: UIColor.secondarySystemBackground) // auto adapts to dark/light mode
+                                : Color.clear
+                        )
+                        .cornerRadius(12)
+                        Spacer()
+                       
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func loadingIndicator(isLoading: Bool,message: String = "") -> some View {
+        self.modifier(LoadingModifier(isLoading: isLoading, loadingText: message))
+    }
+}
